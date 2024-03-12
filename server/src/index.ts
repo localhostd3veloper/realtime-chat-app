d67a9cc7c86ffc31a706900e927476f0a754fa96
localhostd3veloper <gautamanand022@gmail.com>
2024-03-12 21:56:07 +0530

    socket code

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import compression from 'compression';
 import cors from 'cors';
 import morgan from 'morgan';
 import router from './router';
-
+import { Server } from 'socket.io';
 const app = express();
 
 app.use(
@@ -21,6 +21,19 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User Connected : ${socket.id}`);
+  socket.on('new_message', (data) => {
+    socket.broadcast.emit('receive_message', data);
+  });
+});
 
 server.listen(8080, () => {
   console.log('Server Running on http://localhost:8080/');
